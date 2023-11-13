857ba8a7fdf982ab9dfba847b02f241a75255499
Sandaru  De Silva <sandarualoka99@gmail.com>
2023-11-13 15:52:37 +0530

    fetching data to bulk upload

--- src/pages/bulk/Bulk.jsx
+++ src/pages/bulk/Bulk.jsx
@@ -1,6 +1,74 @@
+// import React, { useState } from 'react';
+// import Sidebar from '../../components/sidebar/Sidebar';
+// import axios from 'axios';
+// import './bulk.scss';
+
+// const Bulk = () => {
+//   const [selectedFile, setSelectedFile] = useState(null);
+
+//   const handleFileChange = (event) => {
+//     const file = event.target.files[0];
+//     setSelectedFile(file);
+//   };
+
+//   const handleSubmit = async () => {
+//     try {
+//       if (selectedFile) {
+//         const formData = new FormData();
+//         formData.append('file', selectedFile);
+
+//         // Send the CSV file to your backend
+//         const response = await axios.post('YOUR_BACKEND_API_ENDPOINT', formData, {
+//           headers: {
+//             'Content-Type': 'multipart/form-data',
+//           },
+//         });
+
+//         // Handle the response from the backend as needed
+//         console.log('Backend Response:', response.data);
+//       } else {
+//         console.error('No file selected');
+//       }
+//     } catch (error) {
+//       console.error('Error uploading CSV file:', error);
+//     }
+//   };
+
+//   return (
+//     <div className="bulk">
+//   <Sidebar />
+//   <div className="bulkContainer">
+//     <div className="center-content">
+//       <img
+//         className='logoEmployee'
+//         src='https://imgs.search.brave.com/LBMkd1l_eB7YGFDCkxZo4ymPkHbMcY0RLhi6A7i9G8Y/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTMw/NTI4NjE1My92ZWN0/b3IvbmV3LXRlYW0t/bWVtYmVycy1hYnN0/cmFjdC1jb25jZXB0/LXZlY3Rvci1pbGx1/c3RyYXRpb24uanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPWdB/YWJQelJtcFo4RUFo/MnZ0dUhRSkV1cDZC/MU9aaVFBaXRVRldH/U1dlQTg9'
+//         alt='logoEmployee'
+//       />
+//       <h2 className='bulkHeader'>Bulk Employee Upload</h2>
+//       <div className="button-container">
+//         <input className='browseBtn' type="file" accept=".csv" onChange={handleFileChange} />
+        
+        
+//             <div class="wrap1">
+//                 <button class="button" onClick={handleSubmit}>Submit</button>
+//             </div>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+
+
+//   );
+// };
+
+// export default Bulk;
+
+
+
 import React, { useState } from 'react';
 import Sidebar from '../../components/sidebar/Sidebar';
 import axios from 'axios';
+import Papa from 'papaparse';
 import './bulk.scss';
 
 const Bulk = () => {
@@ -11,53 +79,85 @@ const Bulk = () => {
     setSelectedFile(file);
   };
 
-  const handleSubmit = async () => {
+  const handleFileSubmit = async () => {
     try {
       if (selectedFile) {
-        const formData = new FormData();
-        formData.append('file', selectedFile);
-
-        // Send the CSV file to your backend
-        const response = await axios.post('YOUR_BACKEND_API_ENDPOINT', formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
+        // Parse the CSV file
+        Papa.parse(selectedFile, {
+          complete: async (result) => {
+            // Extract the data from the parsed result
+            const csvData = result.data.filter(entry => entry.employeeid && entry.password);
+    
+            // Format the data to match the desired structure
+            const formattedData = csvData.map(entry => ({
+              employeeid: entry.employeeid,
+              password: entry.password,
+            }));
+    
+            console.log("Formatted Data:", formattedData);
+    
+            // Retrieve the token from local storage
+            const token = localStorage.getItem('accessToken');
+            console.log("Token",token);
+            // Check if the token is available
+            if (!token) {
+              console.error('Token not available');
+              return;
+            }
+    
+            try {
+              // Send the formatted data to your backend with the auth-token in the headers using fetch
+              const response = await fetch('https://1ed8-124-43-9-155.ngrok-free.app/api/users/register', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'auth-token': token,
+                },
+                body: JSON.stringify(formattedData ),
+              });
+    
+              // Handle the response from the backend as needed
+              if (response.ok) {
+                const responseData = await response.json();
+                console.log('Backend Response:', responseData);
+              } else {
+                console.error('Error:', response.statusText);
+              }
+            } catch (error) {
+              console.error('Error:', error.message);
+            }
           },
+          header: true, // Treat the first row as headers
         });
-
-        // Handle the response from the backend as needed
-        console.log('Backend Response:', response.data);
       } else {
         console.error('No file selected');
       }
     } catch (error) {
-      console.error('Error uploading CSV file:', error);
+      console.error('Error processing CSV file:', error);
     }
+    
   };
 
   return (
     <div className="bulk">
-  <Sidebar />
-  <div className="bulkContainer">
-    <div className="center-content">
-      <img
-        className='logoEmployee'
-        src='https://imgs.search.brave.com/LBMkd1l_eB7YGFDCkxZo4ymPkHbMcY0RLhi6A7i9G8Y/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTMw/NTI4NjE1My92ZWN0/b3IvbmV3LXRlYW0t/bWVtYmVycy1hYnN0/cmFjdC1jb25jZXB0/LXZlY3Rvci1pbGx1/c3RyYXRpb24uanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPWdB/YWJQelJtcFo4RUFo/MnZ0dUhRSkV1cDZC/MU9aaVFBaXRVRldH/U1dlQTg9'
-        alt='logoEmployee'
-      />
-      <h2 className='bulkHeader'>Bulk Employee Upload</h2>
-      <div className="button-container">
-        <input className='browseBtn' type="file" accept=".csv" onChange={handleFileChange} />
-        
-        
-            <div class="wrap1">
-                <button class="button" onClick={handleSubmit}>Submit</button>
+      <Sidebar />
+      <div className="bulkContainer">
+        <div className="center-content">
+          <img
+            className='logoEmployee'
+            src='https://imgs.search.brave.com/LBMkd1l_eB7YGFDCkxZo4ymPkHbMcY0RLhi6A7i9G8Y/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTMw/NTI4NjE1My92ZWN0/b3IvbmV3LXRlYW0t/bWVtYmVycy1hYnN0/cmFjdC1jb25jZXB0/LXZlY3Rvci1pbGx1/c3RyYXRpb24uanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPWdB/YWJQelJtcFo4RUFo/MnZ0dUhRSkV1cDZC/MU9aaVFBaXRVRldH/U1dlQTg9'
+            alt='logoEmployee'
+          />
+          <h2 className='bulkHeader'>Bulk Employee Upload</h2>
+          <div className="button-container">
+            <input className='browseBtn' type="file" accept=".csv" onChange={handleFileChange} />
+            <div className="wrap1">
+              <button className="button" onClick={handleFileSubmit}>Submit</button>
             </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-
-
   );
 };
 
