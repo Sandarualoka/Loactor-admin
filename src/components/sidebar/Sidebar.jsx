ebc7498bc47ad4fe2f81d943491bbba6ff5323a8
Sandaru  De Silva <sandarualoka99@gmail.com>
2023-11-14 17:24:54 +0530

    changing the sidebar component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,3 +1,144 @@
+// // import React from 'react';
+// // import "./sidebar.scss";
+// // import LogoutIcon from '@mui/icons-material/Logout';
+// // import { Link } from 'react-router-dom';
+// // import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
+// // import PersonIcon from '@mui/icons-material/Person';
+// // import GroupAddIcon from '@mui/icons-material/GroupAdd';
+// // import FilterAltIcon from '@mui/icons-material/FilterAlt';
+
+// // const Sidebar = () => {
+// //   const handleLogout = () => {
+// //     // Clear sessions, local storage, or perform any other necessary tasks
+// //     localStorage.clear(); // Clear local storage
+// //   };
+
+// //   return (
+// //     <div className='sidebar'>
+// //       <div className="top">
+// //         <Link to="/new" style={{textDecoration: "none"}}>
+// //           <span className='logo'>LOCATOR ADMIN</span>
+// //         </Link>
+// //       </div>
+// //       <hr />
+// //       <div className="center">
+// //         <ul>
+// //           <p className='title'>OPTIONS</p>
+// //           <Link to="/new" style={{textDecoration: "none"}}>
+// //             <li>
+// //               <PersonIcon className="icon" />
+// //               <span >Add New Employee</span>
+// //             </li>
+// //           </Link>
+
+// //           <Link to="/bulk" style={{textDecoration: "none"}}>
+// //             <li>
+// //               <GroupAddIcon className="icon" />
+// //               <span >Add Bulk Employees</span>
+// //             </li>
+// //           </Link>
+
+// //           <Link to="/view" style={{textDecoration: "none"}}>
+// //             <li>
+// //               <FormatListNumberedIcon className="icon" />
+// //               <span >View Employees</span>
+// //             </li>
+// //           </Link>
+
+// //           <Link to="/viewDetails" style={{textDecoration: "none"}}>
+// //             <li>
+// //               <FilterAltIcon className="icon" />
+// //               <span >View Employee Details</span>
+// //             </li>
+// //           </Link>
+
+          
+// //           <li>
+// //             <Link to="/" onClick={handleLogout} style={{textDecoration: "none"}}>
+// //               <LogoutIcon className="icon" />
+// //               <span>Logout</span>
+// //             </Link>
+// //           </li>
+// //         </ul>
+// //       </div>
+      
+// //     </div>
+// //   );
+// // };
+
+// // export default Sidebar;
+
+
+// import React from 'react';
+// import "./sidebar.scss";
+// import LogoutIcon from '@mui/icons-material/Logout';
+// import { Link } from 'react-router-dom';
+// import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
+// import PersonIcon from '@mui/icons-material/Person';
+// import GroupAddIcon from '@mui/icons-material/GroupAdd';
+// import FilterAltIcon from '@mui/icons-material/FilterAlt';
+
+// const Sidebar = () => {
+//   const handleLogout = () => {
+//     // Clear sessions, local storage, or perform any other necessary tasks
+//     localStorage.clear(); // Clear local storage
+//   };
+
+//   return (
+//     <div className='sidebar' style={{ backgroundColor: '  #0041fe ' }}> {/* Add a light background color */}
+//       <div className="top">
+//         <Link to="/new" style={{textDecoration: "none"}}>
+//           <span className='logo'>LOCATOR ADMIN</span>
+//         </Link>
+//       </div>
+//       <hr />
+//       <div className="center">
+//         <ul>
+//           <p className='title'>OPTIONS</p>
+//           <Link to="/new" style={{textDecoration: "none"}}>
+//             <li>
+//               <PersonIcon className="icon" />
+//               <span >Add New Employee</span>
+//             </li>
+//           </Link>
+
+//           <Link to="/bulk" style={{textDecoration: "none"}}>
+//             <li>
+//               <GroupAddIcon className="icon" />
+//               <span >Add Bulk Employees</span>
+//             </li>
+//           </Link>
+
+//           <Link to="/view" style={{textDecoration: "none"}}>
+//             <li>
+//               <FormatListNumberedIcon className="icon" />
+//               <span >View Employees</span>
+//             </li>
+//           </Link>
+
+//           <Link to="/viewDetails" style={{textDecoration: "none"}}>
+//             <li>
+//               <FilterAltIcon className="icon" />
+//               <span >View Employee Details</span>
+//             </li>
+//           </Link>
+
+          
+//           <li>
+//             <Link to="/" onClick={handleLogout} style={{textDecoration: "none"}}>
+//               <LogoutIcon className="icon" />
+//               <span>Logout</span>
+//             </Link>
+//           </li>
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Sidebar;
+
+
 import React from 'react';
 import "./sidebar.scss";
 import LogoutIcon from '@mui/icons-material/Logout';
@@ -14,54 +155,53 @@ const Sidebar = () => {
   };
 
   return (
-    <div className='sidebar'>
+    <div className='sidebar' style={{ backgroundColor: '  #0041fe ' }}> {/* Add a light background color */}
       <div className="top">
         <Link to="/new" style={{textDecoration: "none"}}>
-          <span className='logo'>LOCATOR ADMIN</span>
+          <span className='logo' style={{ color: 'white' }}>LOCATOR ADMIN</span>
         </Link>
       </div>
       <hr />
       <div className="center">
         <ul>
-          <p className='title'>OPTIONS</p>
+          <p className='title' style={{ color: 'white' }}>OPTIONS</p>
           <Link to="/new" style={{textDecoration: "none"}}>
-            <li>
-              <PersonIcon className="icon" />
-              <span >Add New Employee</span>
+            <li style={{ color: 'white' }}>
+              <PersonIcon className="icon" style={{ color: 'white' }}/>
+              <span style={{ color: 'white' }}>Add New Employee</span>
             </li>
           </Link>
 
           <Link to="/bulk" style={{textDecoration: "none"}}>
-            <li>
-              <GroupAddIcon className="icon" />
-              <span >Add Bulk Employees</span>
+            <li style={{ color: 'white' }}>
+              <GroupAddIcon className="icon" style={{ color: 'white' }} />
+              <span style={{ color: 'white' }}>Add Bulk Employees</span>
             </li>
           </Link>
 
           <Link to="/view" style={{textDecoration: "none"}}>
-            <li>
-              <FormatListNumberedIcon className="icon" />
-              <span >View Employees</span>
+            <li style={{ color: 'white' }}>
+              <FormatListNumberedIcon className="icon" style={{ color: 'white' }}/>
+              <span style={{ color: 'white' }}>View Employees</span>
             </li>
           </Link>
 
           <Link to="/viewDetails" style={{textDecoration: "none"}}>
-            <li>
-              <FilterAltIcon className="icon" />
-              <span >View Employee Details</span>
+            <li >
+              <FilterAltIcon className="icon" style={{ color: 'white' }} />
+              <span style={{ color: 'white' }}>View Employee Details</span>
             </li>
           </Link>
 
           
           <li>
-            <Link to="/" onClick={handleLogout} style={{textDecoration: "none"}}>
-              <LogoutIcon className="icon" />
-              <span>Logout</span>
+            <Link to="/" onClick={handleLogout} style={{textDecoration: "none", color: 'white' }}>
+              <LogoutIcon className="icon" style={{ color: 'white' }} />
+              <span style={{ color: 'white' }}>Logout</span>
             </Link>
           </li>
         </ul>
       </div>
-      
     </div>
   );
 };
--- src/pages/viewDetails/ViewDetails.jsx
+++ src/pages/viewDetails/ViewDetails.jsx
@@ -1,165 +1,4 @@
 
-// import React, { useEffect, useState } from 'react';
-// import Table from '@mui/material/Table';
-// import TableBody from '@mui/material/TableBody';
-// import TableCell from '@mui/material/TableCell';
-// import TableHead from '@mui/material/TableHead';
-// import TableRow from '@mui/material/TableRow';
-// import TablePagination from '@mui/material/TablePagination';
-// import TextField from '@mui/material/TextField';
-// import IconButton from '@mui/material/IconButton';
-// import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
-// import Sidebar from '../../components/sidebar/Sidebar';
-// import './viewDetails.scss';
-
-// const ViewDetails = () => {
-//   const [employeeData, setEmployeeData] = useState([]);
-//   const [filteredData, setFilteredData] = useState([]);
-//   const [page, setPage] = useState(0);
-//   const [rowsPerPage, setRowsPerPage] = useState(5);
-//   const [startDate, setStartDate] = useState(null);
-//   const [endDate, setEndDate] = useState(null);
-
-//   useEffect(() => {
-//     // Fetch employee data from your backend API
-//     // Replace 'yourApiEndpoint' with the actual endpoint
-
-//     // MEKATA ADD KARAPIYA ENDPOINT EKA---->>>
-
-//     fetch('yourApiEndpoint')
-//       .then(response => response.json())
-//       .then(data => {
-//         setEmployeeData(data);
-//         setFilteredData(data);
-//       })
-//       .catch(error => console.error('Error fetching data:', error));
-//   }, []); // Empty dependency array ensures the effect runs only once on mount
-
-//   const handleChangePage = (event, newPage) => {
-//     setPage(newPage);
-//   };
-
-//   const handleChangeRowsPerPage = (event) => {
-//     setRowsPerPage(parseInt(event.target.value, 10));
-//     setPage(0);
-//   };
-
-//   const handleStartDateChange = (event) => {
-//     setStartDate(new Date(event.target.value));
-//     filterData(new Date(event.target.value), endDate);
-//   };
-
-//   const handleEndDateChange = (event) => {
-//     setEndDate(new Date(event.target.value));
-//     filterData(startDate, new Date(event.target.value));
-//   };
-
-//   const filterData = (start, end) => {
-//     const filtered = employeeData.filter((employee) => {
-//       const inTime = new Date(employee.inTime);
-//       const outTime = new Date(employee.outTime);
-
-//       return (!start || inTime >= start) && (!end || outTime <= end);
-//     });
-
-//     setFilteredData(filtered);
-//   };
-
-//   const columns = [
-//     {
-//       name: 'employeeId',
-//       label: 'Employee ID',
-//     },
-//     {
-//       name: 'inTime',
-//       label: 'In Time',
-//     },
-//     {
-//       name: 'outTime',
-//       label: 'Out Time',
-//     },
-//     {
-//       name: 'inLocation',
-//       label: 'In Location',
-//     },
-//     {
-//       name: 'outLocation',
-//       label: 'Out Location',
-//     },
-//   ];
-
-//   return (
-//     <div className="viewDetails">
-//       <Sidebar />
-//       <div className="viewDetailsContainer">
-//         <div className="viewDetailsImg bounceEffect">
-//           <img src='https://img.freepik.com/free-vector/business-team-working-project-project-management-business-analysis-planning-brainstorming-research-consulting-motivation-concept-vector-isolated-illustration_335657-2244.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699228800&semt=ais' alt='viewDImage' />
-//         </div>
-
-//         {/* Date Pickers */}
-//         <div className="dateFilterContainer">
-//           <TextField
-//           className='textfiled1'
-//             id="startDate"
-//             label="Start Date"
-//             type="date"
-//             value={startDate ? startDate.toISOString().split('T')[0] : ''}
-//             onChange={handleStartDateChange}
-//             InputLabelProps={{
-//               shrink: true,
-//             }}
-//           />
-//           <TextField
-//             id="endDate"
-//             label="End Date"
-//             type="date"
-//             value={endDate ? endDate.toISOString().split('T')[0] : ''}
-//             onChange={handleEndDateChange}
-//             InputLabelProps={{
-//               shrink: true,
-//             }}
-//           />
-//           <IconButton color="primary">
-//             <CalendarTodayIcon />
-//           </IconButton>
-//         </div>
-
-//         {/* Table */}
-//         <Table>
-//           <TableHead>
-//             <TableRow>
-//               {columns.map((column) => (
-//                 <TableCell key={column.name}>{column.label}</TableCell>
-//               ))}
-//             </TableRow>
-//           </TableHead>
-//           <TableBody>
-//             {filteredData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((employee) => (
-//               <TableRow key={employee.employeeId}>
-//                 {columns.map((column) => (
-//                   <TableCell key={column.name}>{employee[column.name]}</TableCell>
-//                 ))}
-//               </TableRow>
-//             ))}
-//           </TableBody>
-//         </Table>
-
-//         {/* Table Pagination */}
-//         <TablePagination
-//           rowsPerPageOptions={[5, 10, 25]}
-//           component="div"
-//           count={filteredData.length}
-//           rowsPerPage={rowsPerPage}
-//           page={page}
-//           onPageChange={handleChangePage}
-//           onRowsPerPageChange={handleChangeRowsPerPage}
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ViewDetails;
 
 import React, { useEffect, useState } from 'react';
 import Table from '@mui/material/Table';
@@ -264,13 +103,17 @@ const ViewDetails = () => {
     <div className="viewDetails">
       <Sidebar />
       <div className="viewDetailsContainer">
-        <div className="viewDetailsImg">
+       
+        <div className="viewDetailsImg bounceEffect">
           <img src='https://img.freepik.com/free-vector/business-team-working-project-project-management-business-analysis-planning-brainstorming-research-consulting-motivation-concept-vector-isolated-illustration_335657-2244.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699228800&semt=ais' alt='viewDImage' />
         </div>
+
+       
 
         {/* Date Pickers */}
         <div className="dateFilterContainer">
           <TextField
+          className='input1'
             id="startDate"
             label="Start Date"
             type="date"
@@ -298,6 +141,7 @@ const ViewDetails = () => {
         {/* Search Bar */}
         <div className="searchBarContainer">
           <TextField
+          className='input1'
             id="search"
             label="Search by ID"
             value={searchTerm}
@@ -344,3 +188,4 @@ const ViewDetails = () => {
 };
 
 export default ViewDetails;
+
